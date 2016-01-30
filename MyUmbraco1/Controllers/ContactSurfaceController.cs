553dc58dc61edbc7c10f2ccb9f50b2e4d50eb540
ractor1 <caroline@ractives.com>
2016-01-29 18:39:36 -1000

    first commit

---
+++ MyUmbraco1/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using MyUmbraco1.Models;
+
+namespace MyUmbraco1.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public ActionResult ShowForm()
+        {
+            ContactDataModel mmodel =  new ContactDataModel();
+            return PartialView("ContactForm", mmodel);
+        }
+
+        public ActionResult HandleFromPost(ContactDataModel model)
+        {
+            var newEmail = Services.ContentService.CreateContent(model.ID + model.firstname, CurrentPage.Id, "ContactPage");
+            newEmail.SetValue("emailfrom", model.emailfrom);
+            newEmail.SetValue("firstname", model.firstname);
+            newEmail.SetValue("lastname", model.lastname);
+            Services.ContentService.SaveAndPublishWithStatus(newEmail);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ MyUmbraco1/Models/ContactDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyUmbraco1.Models
+{
+    public class ContactDataModel
+    {
+        public int ID { get; set; }
+        public string emailfrom { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+    }
+}
